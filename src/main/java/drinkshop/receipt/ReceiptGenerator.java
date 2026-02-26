6b12aa777cc391bb1eda4185fa9c9070bab35a4d
borospatri <boros.patri@gmail.com>
2026-02-26 20:45:05 +0200

    Lab1

--- src/main/java/drinkshop/receipt/ReceiptGenerator.java
+++ src/main/java/drinkshop/receipt/ReceiptGenerator.java
@@ -7,14 +7,20 @@ import drinkshop.domain.Product;
 import java.util.List;
 
 public class ReceiptGenerator {
-    public static String generate(Order o, List<Product> products) {
+    public String generate(Order order) {
         StringBuilder sb = new StringBuilder();
-        sb.append("===== BON FISCAL =====\n").append("Comanda #").append(o.getId()).append("\n");
-        for (OrderItem i : o.getItems()) {
-            Product p = products.stream().filter((p1)->i.getProduct().getId()==p1.getId()).toList().get(0);
-            sb.append(p.getNume()+": ").append(p.getPret()).append(" x ").append(i.getQuantity()).append(" = ").append(i.getTotal()).append(" RON\n");
+
+        sb.append("RECEIPT").append('\n');
+        sb.append("Order ID: ").append(order.getId()).append('\n');
+
+        for (OrderItem item : order.getItems()) {
+            sb.append(item.getProduct().getNume())
+                    .append(" x ")
+                    .append(item.getQuantity())
+                    .append('\n');
         }
-        sb.append("---------------------\nTOTAL: ").append(o.getTotalPrice()).append(" RON\n=====================\n");
+
+        sb.append("Total: ").append(order.getTotalPrice()).append('\n');
         return sb.toString();
     }
 }
